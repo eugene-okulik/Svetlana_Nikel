2968197a774f32c393049d9aeaf954f5284d1525
Svetlana Nikel <svetlananikel3@gmail.com>
2025-02-04 13:13:26 +0500

    Доработка по третьему уроку

--- homework/Svetlana_Nikel/homework_3/task_2.py
+++ homework/Svetlana_Nikel/homework_3/task_2.py
@@ -1,3 +1,3 @@
 x = 2
 y = 4
-print(x - y / 1 + x*y)
+print(x - y / 1 + x * y)
--- homework/Svetlana_Nikel/homework_3/task_3.py
+++ homework/Svetlana_Nikel/homework_3/task_3.py
@@ -1,5 +1,5 @@
 num = [50, 42]
-ar_average = sum(num)/len(num)
-geom_average = (num[0] * num[1]) ** (1/len(num))
+ar_average = sum(num) / len(num)
+geom_average = (num[0] * num[1]) ** (1 / len(num))
 print(f'Среднее арифметическое чисел {num} равно {ar_average}')
 print(f'Среднее геометрическое чисел {num} равно {geom_average}')
