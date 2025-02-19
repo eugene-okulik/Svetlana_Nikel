c73dd42046cfcf5e6c79ace83b6dc6a7f63f6260
Svetlana Nikel <svetlananikel3@gmail.com>
2025-02-19 14:15:46 +0500

    Исправление по домашнему заданию к уроку 4

--- homework/Svetlana_Nikel/homework_4/task_1.py
+++ homework/Svetlana_Nikel/homework_4/task_1.py
@@ -11,16 +11,16 @@ my_dict = {
     'set': {1.5, 1.8, 2.0, 7.9, 9.3}
 }
 
-print(my_dict['tuple'][-1]) #‘tuple’ - выведите на экран последний элемент
+print(my_dict['tuple'][-1]) # tuple - выведите на экран последний элемент
 
-my_dict['list'].append(6) # 'list' - добавьте в конец списка еще один элемент
-my_dict['list'].pop(1) #'list' - удалите второй элемент списка
+my_dict['list'].append(6) # list - добавьте в конец списка еще один элемент
+my_dict['list'].pop(1) # list - удалите второй элемент списка
 
-my_dict['dict']['i am a tuple'] = 'new_value' #'dict' - добавьте элемент с ключом ('i am a tuple',) и любым значением
-my_dict['dict'].pop('name_3') #'dict' - удалите какой-нибудь элемент
+my_dict['dict']['i am a tuple'] = 'new_value' # dict - добавьте элемент с ключом ('i am a tuple',) и любым значением
+my_dict['dict'].pop('name_3') # dict - удалите какой-нибудь элемент
 
-my_dict['set'].add(10.15) #'set' - добавьте новый элемент в множество
-my_dict['set'].discard(2.0) #'set' - удалите элемент из множества
+my_dict['set'].add(10.15) # set - добавьте новый элемент в множество
+my_dict['set'].discard(2.0) # set - удалите элемент из множества
 
 print(my_dict)
 
