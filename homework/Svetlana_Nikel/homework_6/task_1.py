fb75480c613a83f29cacbc3e44924681b5a51b73
Svetlana Nikel <svetlananikel3@gmail.com>
2025-03-04 17:24:56 +0500

    Домашнее задание к уроку 6

---
+++ homework/Svetlana_Nikel/homework_6/task_1.py
@@ -0,0 +1,10 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+words = text.split()
+fin_words = []
+for word in words:
+    if word[-1] in ['.', ',']:  # ищем слова в конце которых есть точка или запятая
+        new_words = word[:-1] + 'ing' + word[-1] # добавляем 'ing' и знак препинания
+    else:
+        new_words = word + 'ing'  # прибавляем приставку 'ing' во все остальные слова
+    fin_words.append(new_words)  # добавляем слова в новый список
+print(' '.join(fin_words))
