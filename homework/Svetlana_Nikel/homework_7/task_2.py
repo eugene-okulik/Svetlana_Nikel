180bf37a5f92aba73d07368bc24ae7f518149444
Svetlana Nikel <svetlananikel3@gmail.com>
2025-03-13 18:08:34 +0500

    Д/з к уроку 7, борьба с lint

--- homework/Svetlana_Nikel/homework_7/task_2.py
+++ homework/Svetlana_Nikel/homework_7/task_2.py
@@ -1,5 +1,8 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
-def print_words(words):
-    for key, valye in words.items():
+
+def print_words(word):
+    for key, valye in word.items():
         print(key * valye)
+
+
 print_words(words)
