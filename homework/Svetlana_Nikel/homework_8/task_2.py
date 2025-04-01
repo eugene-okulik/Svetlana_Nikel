89675cb5be66eb681f04e273d19b6e9f8c2f9263
Svetlana Nikel <svetlananikel3@gmail.com>
2025-04-01 15:09:17 +0500

    Домашнее задание к 8 уроку

---
+++ homework/Svetlana_Nikel/homework_8/task_2.py
@@ -0,0 +1,18 @@
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+count = 0
+for number in fibonacci_generator():
+    if count == 4:
+        print(f"Пятое число Фибоначчи: {number}")
+    elif count == 199:
+        print(f"Двухсотое число Фибоначчи: {number}")
+    elif count == 999:
+        print(f"Тысячное число Фибоначчи: {number}")
+    elif count == 99999:
+        print(f"Стотысячное число Фибоначчи: {number}")
+        break
+    count += 1
