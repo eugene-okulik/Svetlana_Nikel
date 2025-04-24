f3b15e10264cca4c9991c45068e67b05d0cc9cdc
Svetlana Nikel <svetlananikel3@gmail.com>
2025-04-24 12:24:37 +0500

    Домашнее задание к уроку 10

---
+++ homework/Svetlana_Nikel/homework_10/task_2.py
@@ -0,0 +1,13 @@
+def repeat_me(count=1):
+    def decorator(func):
+        def wrapper(*args):
+            for _ in range(count):
+                func(*args)
+        return wrapper
+    return decorator
+
+@repeat_me(count=2)
+def example(text):
+    print(text)
+
+example('print me')
