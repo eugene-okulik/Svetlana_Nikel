badbe8f3ee1a563ab50f288950ee12d48f051127
Svetlana Nikel <svetlananikel3@gmail.com>
2025-07-23 17:37:09 +0500

    Домашнее задание к уроку 12

---
+++ homework/Svetlana_Nikel/homework_12/task_1.py
@@ -0,0 +1,72 @@
+class Flowers:
+    def __init__(self, name, color, price, lifespan):
+        self.name = name
+        self.color = color
+        self.price = price
+        self.lifespan = lifespan
+
+    def __repr__(self):
+        return f'{self.name} {self.color}, стоит {self.price} рублей, срок жизни {self.lifespan} дней'
+
+
+class Gortensia(Flowers):
+    def __init__(self, color, price):
+        super().__init__('Гортензия', color, price, 10)
+
+
+class Giacint(Flowers):
+    def __init__(self, color, price):
+        super().__init__('Гиацинт', color, price, 8)
+
+
+class Rosa(Flowers):
+    def __init__(self, color, price):
+        super().__init__('Роза', color, price, 5)
+
+
+class Buket:
+    def __init__(self):
+        self.flowers = []
+
+    def add_flowers(self, flowers):
+        self.flowers.append(flowers)
+
+    def total(self):
+        total = 0
+        for flower in self.flowers:
+            total += flower.price
+        return total
+
+    def lifespan(self):
+        total_days = 0
+        for flower in self.flowers:
+            total_days += flower.lifespan
+        average = total_days / len(self.flowers)
+        return round(average, 0)
+
+    def color(self, color):
+        found_flowers = []
+        for flower in self.flowers:
+            if flower.color == color:
+                found_flowers.append(flower)
+        return found_flowers
+
+
+flower1 = Giacint('желтый', 700)
+flower2 = Gortensia('белый', 1000)
+flower3 = Rosa('красная', 500)
+
+new_buket = Buket()
+new_buket.add_flowers(flower1)
+new_buket.add_flowers(flower2)
+new_buket.add_flowers(flower3)
+
+print('Букет стоит', new_buket.total(), 'руб.')
+print('Букет будет свеж', new_buket.lifespan(), 'дней')
+
+white_flowers = new_buket.color('белый')
+if white_flowers:
+    print("Белый цветок в букете -", white_flowers[0].name)
+else:
+    print("В букете нет белых цветов")
+
