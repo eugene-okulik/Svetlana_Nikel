5fe5bdccbc0e0974afbb52cf33227b411345b3a9
Svetlana Nikel <svetlananikel3@gmail.com>
2025-07-24 11:32:11 +0500

    Домашнее задание к уроку 12, добавила метод сортировки

--- homework/Svetlana_Nikel/homework_12/task_1.py
+++ homework/Svetlana_Nikel/homework_12/task_1.py
@@ -51,15 +51,28 @@ class Buket:
                 found_flowers.append(flower)
         return found_flowers
 
+    def sort(self, sort_key = 'name'):
+        if sort_key == 'color':
+            self.flowers.sort(key=lambda flower: flower.color)
+        elif sort_key == 'price':
+            self.flowers.sort(key=lambda flower: flower.price)
+        elif sort_key == 'lifespan':
+            self.flowers.sort(key=lambda flower: flower.lifespan)
+        else:
+            self.flowers.sort(key=lambda flower: flower.name)
+
+
 
 flower1 = Giacint('желтый', 700)
 flower2 = Gortensia('белый', 1000)
-flower3 = Rosa('красная', 500)
+flower3 = Rosa('красный', 500)
+flower4 = Rosa('белый', 900)
 
 new_buket = Buket()
 new_buket.add_flowers(flower1)
 new_buket.add_flowers(flower2)
 new_buket.add_flowers(flower3)
+new_buket.add_flowers(flower4)
 
 print('Букет стоит', new_buket.total(), 'руб.')
 print('Букет будет свеж', new_buket.lifespan(), 'дней')
@@ -69,3 +82,12 @@ if white_flowers:
     print("Белый цветок в букете -", white_flowers[0].name)
 else:
     print("В букете нет белых цветов")
+
+new_buket.sort(sort_key='price')
+print("Сортировка по цене:", ", ".join(f"{flower.name} цвет {flower.color} : {flower.price} рублей" for flower in new_buket.flowers))
+
+new_buket.sort(sort_key='color')
+print("Сортировка по цвету:", ", ".join(f"{flower.name} {flower.color} цвет" for flower in new_buket.flowers))
+
+new_buket.sort(sort_key='name')
+print("Сортировка по названию:", ", ".join(f"{flower.name}" for flower in new_buket.flowers))
